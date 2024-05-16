dea688b7421df671a5320a82d8995bb486768f38
Hunter Mills <huntermills@Hunters-MacBook-Pro.local>
2024-05-15 22:34:27 -0400

    axis interface

--- srcs/hdl/cs4344/cs4344.vhd
+++ srcs/hdl/cs4344/cs4344.vhd
@@ -17,10 +17,11 @@ use ieee.math_real.all;
 entity cs4344 is
   port(
     s_axis_resetn : in std_logic;                     -- Synchronous axis reset (active low)
-    s_axis_clk    : in std_logic; 										-- AXIS System Clock (125MHz)
-    s_axis_tdata  : in std_logic_vector(31 downto 0);	-- 32 bit input data
+    s_axis_clk    : in std_logic;                     -- AXIS System Clock (125MHz)
+    s_axis_tdata  : in std_logic_vector(31 downto 0); -- 32 bit input data
     s_axis_tvalid : in std_logic;                     -- AXIS Valid Signal
     s_axis_tready : out std_logic;                    -- AXIS Ready Signal
+    s_axis_tlast  : in std_logic;                     -- AXIS Last Signal
     sdata         : out std_logic;                    -- Serial Data for I2S stream
     lrclk         : out std_logic;                    -- Left Right 48kHz Clk
     sclk          : out std_logic;                    -- Serial 3.125MHz Clk
@@ -42,13 +43,16 @@ architecture behav of cs4344 is
   signal mclk_125clk_count  : natural range 0 to 4    := 0;
   signal sclk_125clk_count  : natural range 0 to 19   := 0;
   signal lrclk_125clk_count : natural range 0 to 1279 := 0;
+  signal frame_count        : natural range 0 to 63   := 0; --! SCLK per LRCLK Period
 
   --! Data Counter
   signal data_count : natural range 0 to 23 := 23;
 
   --! Register Signals
   signal data_word  : std_logic_vector(31 downto 0);
-  signal valid_rise : std_logic;
+  signal tvalid_reg : std_logic;
+  signal tlast_reg  : std_logic;
+  signal ready      : std_logic;
 
 begin
 
@@ -111,40 +115,64 @@ begin
     if rising_edge(s_axis_clk) then
       if s_axis_resetn = '0' then
         data_count  <= 23;
-      elsif sclk = '1' and sclk_125clk_count = 19 then
-        data_count  <= data_count - 1;
+      elsif lrclk_reg = '0' and lrclk_s = '1' and data_count > 0 then
+        data_count <= data_count - 1;
+      elsif lrclk_reg = '1' and lrclk_s = '0' and data_count > 0 then
+        data_count <= data_count - 1;
+      else
+        data_count <= 0;
+      end if;
+    end if;
+  end process;
 
   -- ------------------------------------------------
   -- Registers
   -- ------------------------------------------------
-  reg_valid_p : process(s_axis_clk)
+  --! RisingEdge Detector
+  reg_axis_p : process(s_axis_clk)
   begin
     if rising_edge(s_axis_clk) then
       if s_axis_resetn = '0' then
-        valid_rise  <= '0';
+        tvalid_reg  <= '0';
+        tlast_reg   <= '0';
+        lrclk_reg   <= '0';
       else
-        valid_rise  <= s_axis_tvalid;
+        tvalid_reg  <= s_axis_tvalid;
+        tlast_reg   <= s_axis_tlast;
+        lrclk_reg   <= lrclk_s;
       end if;
     end if;
   end process;
 
-  reg_data_p : process(s_axis_clk)
+  --! Latch Left and Right channel data
+  latch_data_p : process(s_axis_clk)
   begin
     if rising_edge(s_axis_clk) then
       if s_axis_resetn = '0' then
-        data_word <= (others => '0');
-        s_axis_tready <= '1';
-      elsif valid_rise = '0' and s_axis_tvalid = '1' and data_count = 0 then  --! Rising Edge of tvalid and start of data
-        data_word <= s_axis_tdata;
-        s_axis_tready <= '0';
-      elsif valid_rise = '1' and s_axis_tvalid = '0' and data_count = 31 then --! Falling Edge of tvalid and end of data
-          data_word <= s_axis_tdata;
-          s_axis_tready <= '1';
+        ldata <= (others => '0');
+        rdata <= (others => '0');
+      elsif tvalid_reg = '0' and s_axis_tvalid = '1' then
+        ldata <= s_axis_tdata;
+        ldata_valid <= '1';
+      elsif tlast_reg = '0' and s_axis_tlast = '1' then
+        rdata <= s_axis_tdata;
+        rdata_valid <= '1';
+      elsif ldata_valid = '1' and data_count = 0 then
+        ldata_valid <= '0';
+      elsif rdata_valid = '1' and data_count = 0 then
+        rdata_valid <= '0';
       end if;
     end if;
   end process;
+
+  -- ------------------------------------------------
+  -- Signal Assignments
+  -- ------------------------------------------------
+  --! Process to assign sdata
 
   mclk <= mclk_s;
   sclk <= sclk_s;
   lrclk <= lrclk_s;
+  s_axis_tready <= ready;
+
 end behav;
